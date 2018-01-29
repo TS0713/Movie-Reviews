a98e7477f034fa733b44ffa93b78362b3e4ab3c5
Pavan Satya <tsp.0713@gmail.com>
2018-01-29 04:38:21 -0800

    Add files via upload

---
+++ imdb_reviews.py
@@ -0,0 +1,37 @@
+################# IMDB reviews extraction ######################## Time Taking process as this program is operating the web page while extracting 
+############# the data we need to use time library in order sleep and make it to extract for that specific page 
+#### We need to install selenium for python
+#### pip install selenium
+#### time library to sleep the program for few seconds 
+from selenium import webdriver
+browser = webdriver.Chrome()
+from bs4 import BeautifulSoup as bs
+#page = "http://www.imdb.com/title/tt0944947/reviews?ref_=tt_urv"
+page = "http://www.imdb.com/title/tt6294822/reviews?ref_=tt_urv"
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotVisibleException
+browser.get(page)
+import time
+reviews = []
+i=1
+while (i>0):
+    i=i+25
+    try:
+        button = browser.find_element_by_xpath('//*[@id="load-more-trigger"]')
+        button.click()
+        time.sleep(5)
+        ps = browser.page_source
+        soup=bs(ps,"html.parser")
+        rev = soup.findAll("div",attrs={"class","text"})
+        reviews.extend(rev)
+    except NoSuchElementException:
+        break
+    except ElementNotVisibleException:
+        break
+        
+
+##### If we want only few recent reviews you can either press cntrl+c to break the operation in middle but the it will store 
+##### Whatever data it has extracted so far #######
+len(reviews)
+len(list(set(reviews)))
+
